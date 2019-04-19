24a1c9d3e0e5ee5c55d7230edfe0aa89aae30840
NormXU <nxu8@jhu.edu>
2019-04-18 23:54:01 -0400

    Final Integration

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Self-Driving-Dev
+![](https://github.com/NormXU/Self-Driving-Dev/raw/master/Doc/demo2.gif)
--- untitled0.py
+++ untitled0.py
@@ -9,25 +9,26 @@ import os
 import os.path
 import cv2
 # %%
-def DistanceCompute(depthPath, position, idx, showFlag = True):
-    depth = cv2.imread(depthPath,0)
+def DistanceCompute(depthPath, position, idx, threshold = 90, showFlag = True):
+    depthOrigin = cv2.imread(depthPath,0)
     for i in range(len(position[idx])):
         top = position[idx][i][0]
         left = position[idx][i][1]
         bottom = position[idx][i][2]
         right = position[idx][i][3]
-        depth = cv2.rectangle(depth,(top,left),(bottom,right),(255,255,0),3)
-    if showFlag:
-        plt.imshow(depth,cmap='gray')
-        plt.show()
+        crop_Image = depthOrigin[left:right,top:bottom]
+        depth_distance = int(crop_Image.mean())
+        #print(depth_distance)
+        
+        
+        #depth = cv2.rectangle(depthOrigin,(top,left),(bottom,right),(255,255,0),3)
+         
+        #plt.imsave(os.path.join('./data/crops2/depthCrop\', "{}_disp.png".format(i)), depth, cmap='gray')
+        if showFlag and depth_distance > threshold:
+            plt.imshow(crop_Image,cmap='gray')
+            plt.show()
     
     
-Depthdata_path = 'yolo_util/crops2/depth/'
-depthimgs = glob(os.path.join(Depthdata_path, "*.png"))
-for i in range(len(depthimgs)):
-    depthPath = depthimgs[i]
-    DistanceCompute(depthPath, results, i, showFlag = True)
-
 # %%
 import sys
 import argparse
@@ -40,3 +41,10 @@ results = detect_multiple(YOLO(), 'yolo_util/crops2/original/', "")
 img_path = 'yolo_util/crops2/original/'
 imgs = glob(os.path.join(img_path, "*.png"))
 results[0]
+#%%
+Depthdata_path = 'yolo_util/crops2/depth/'
+#Depthdata_path = 'yolo_util/crops2/original/'
+depthimgs = glob(os.path.join(Depthdata_path, "*.png"))
+for i in range(len(depthimgs)):
+    depthPath = depthimgs[i]
+    DistanceCompute(depthPath, results, i, showFlag = True)
